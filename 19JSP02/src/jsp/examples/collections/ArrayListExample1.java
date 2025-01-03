bdad414cc3bf5942aac079c092e7983c24b67226
kulloc2 <139840191+kulloc2@users.noreply.github.com>
2025-01-03 23:54:34 +0700

    Update ArrayListExample1.java
    fsd13

--- 19JSP02/src/jsp/examples/collections/ArrayListExample1.java
+++ 19JSP02/src/jsp/examples/collections/ArrayListExample1.java
@@ -17,7 +17,7 @@ public class ArrayListExample1 {
         // Add objects to arraylist
         list.add("Java111");
         list.add("C+213+");
-        list.add("PHP");
+        list.add("PHP123");
         list.add("Java");
         // Show list through Iterator
         Iterator<String> itr = list.iterator();
