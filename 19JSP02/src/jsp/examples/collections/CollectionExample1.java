8bee31315397b51a61dd3b0d3858fa3e974e3d74
kulloc2 <139840191+kulloc2@users.noreply.github.com>
2025-01-15 10:14:40 +0700

    Update CollectionExample1.java
    ff2

--- 19JSP02/src/jsp/examples/collections/CollectionExample1.java
+++ 19JSP02/src/jsp/examples/collections/CollectionExample1.java
@@ -21,7 +21,7 @@ public class CollectionExample1 {
         arrayList.add("PHP");
         arrayList.add("Python1");
         arrayList.add("Java1");
-        arrayList.add("C++");
+        arrayList.add("C++"e);
         System.out.println("Các phần tử của ArrayList");
         System.out.print("\t" + arrayList + "\n");
  
