67c3d2a1ef14796feed55bc57900e38fc7fe6030
kulloc2 <139840191+kulloc2@users.noreply.github.com>
2025-01-20 16:02:28 +0700

    Update LinkedListExample1.java
    ds2

--- 19JSP02/src/jsp/examples/collections/LinkedListExample1.java
+++ 19JSP02/src/jsp/examples/collections/LinkedListExample1.java
@@ -15,7 +15,7 @@ public class LinkedListExample1 {
         // Create linkedList
         LinkedList<String> linkedList = new LinkedList<String>();
         // Add objects to linkedList
-        linkedList.add("Java123");
+        linkedList.add("Java121233");
         linkedList.add("C++");
         linkedList.add("PHP");
         linkedList.add("Java");
